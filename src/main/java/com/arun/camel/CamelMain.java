d2843b6b58d0d37b0cbfa654d9679d265f1d32d6
jollyarun <jollyarun@gmail.com>
2013-08-04 22:54:51 +0530

    Commit Camel Example

---
+++ src/main/java/com/arun/camel/CamelMain.java
@@ -0,0 +1,56 @@
+package com.arun.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.file.FileComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class CamelMain {
+
+	public void fileComponentExample() throws Exception {
+
+		/* Creating the camel context */
+		DefaultCamelContext camelContext = new DefaultCamelContext();
+		camelContext.addRoutes(new RouteBuilder() {
+
+			@Override
+			public void configure() throws Exception {
+				from("file:camel/test1").to("file:camel/test2");
+
+			}
+		});
+
+		/* Starting the context */
+		camelContext.start();
+		System.out
+				.println("Context started...Start Putting your files...Quick quick");
+		Thread.sleep(60000);
+		System.out.println("Context stopped");
+
+	}
+
+	public void pingGoogleExample() throws Exception {
+
+		DefaultCamelContext camelContext = new DefaultCamelContext();
+		camelContext.addRoutes(new RouteBuilder() {
+
+			@Override
+			public void configure() throws Exception {
+
+				from("timer://foo?fixedRate=true&delay=0&period=10000").to(
+						"http://www.google.com").to(
+						"file:camel/google/test.html?autoCreate=true");
+			}
+		});
+
+		camelContext.start();
+		System.out.println("Camel Context started...");
+		Thread.sleep(60000);
+		System.out.println("Context stopped...");
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		CamelMain camelMain = new CamelMain();
+		camelMain.pingGoogleExample();
+	}
+}
